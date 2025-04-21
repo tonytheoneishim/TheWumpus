98a034045e1a838aa4bf72821cf867a871b14cd8
86pig <1117489@apps.nsd.org>
2025-04-21 15:04:37 -0700

    Test (again i think i failed the first time) !!

--- Console App Test - Highscore/Program.cs
+++ Console App Test - Highscore/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("this is a test push, for malkasian...")
+            Console.WriteLine("this is a test push, for malkasian...");
         }
     }
 }
