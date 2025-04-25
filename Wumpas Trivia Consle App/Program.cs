33bfe18ef93db5941693ef6a54612e3bdf1609c1
tonytheoneishim <2034264@apps.nsd.org>
2025-04-25 14:53:06 -0700

    added trivia project to repo

---
+++ Wumpas Trivia Consle App/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Wumpas_Trivia_Consle_App
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            static bool Question()
+            {
+                bool RightOrWrong = false;
+                string[,] Questions = { { "What year did the Berlin Wall fall?","1986" },
+                { "Who, in 1903, was the first woman to win a Nobel Prize?", "Marie Skłodowska-Curie" } };
+                Random rnd = new Random();
+                int index = rnd.Next(0, Questions.GetLength(0));
+
+                Console.WriteLine(Questions[index, 0]);
+                string input = Console.ReadLine();
+                string answer = Questions[index, 1];
+                if (input == answer)
+                {
+                    Console.WriteLine("You got it right");
+                    RightOrWrong = true;
+
+                }
+                else { Console.WriteLine("You got it wrong"); RightOrWrong = false; }
+                return RightOrWrong;
+            }
+            Console.WriteLine(Question());
+            
+
+
+
+
+        }
+    }
+}
