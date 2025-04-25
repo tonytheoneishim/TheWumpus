e5b61a7f32d73e01948e2cb426026856f6fe4e34
tonytheoneishim <2034264@apps.nsd.org>
2025-04-25 14:58:58 -0700

    add the trivia project for the class

---
+++ Trivia/TriviaQuestions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia
+{
+    public class TriviaQuestions
+    {
+        //bool RightOrWrong = false;
+        //string[,] Questions = { { "What year did the Berlin Wall fall?","1986" },
+        //{ "Who, in 1903, was the first woman to win a Nobel Prize?", "Marie Skłodowska-Curie" } };
+        //Random rnd = new Random();
+        //int index = rnd.Next(0, Questions.GetLength(0));
+
+        //Console.WriteLine(Questions[index, 0]);
+
+        //    string input = Console.ReadLine();
+
+        
+
+        //string answer = Questions[index, 1];
+        //    if (input == answer)
+        //    {
+        //        Console.WriteLine("You got it right");
+        //        RightOrWrong = true;
+
+        //    }
+        //    else { Console.WriteLine("You got it wrong"); RightOrWrong = false; }
+        //    return RightOrWrong;
+
+
+    }
+}
