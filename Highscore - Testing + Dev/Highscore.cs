f5eaf6ad747f0f560ba7fb50d72db025d4dda279
86pig <1117489@apps.nsd.org>
2025-05-01 15:00:43 -0700

    Updated write to file and save to file

--- Highscore - Testing + Dev/Highscore.cs
+++ Highscore - Testing + Dev/Highscore.cs
@@ -7,36 +7,40 @@ public class Highscore
 {
    
     const string DATAFILE = "highscores.csv";
-
+    string converted = "";
 
 
 
     public Highscore()
     {
         PlayerName = string.Empty;
-        PlayerFinalScore = 
+        PlayerFinalScore = 0;
+        CaveType = string.Empty ;
     }
-    public Highscore(string n, int s)
+    public Highscore(string n, int s, string c)
     {
         PlayerName = n;
         PlayerFinalScore = s;
+        CaveType = c;
 
     }
     public string PlayerName {get; set;}
     public int PlayerFinalScore {get; set;}
+    public string CaveType {get; set;}  
     public List<Highscore> PlayerList = new List<Highscore>();
 
-    public string ConvertTheFinalScoreToAString()
-    {
-        string outp = PlayerFinalScore.ToString();
-        return outp;
-    }
+    //public string ConvertTheFinalScoreToAString()
+    //{
+    //    converted = PlayerFinalScore.ToString();
+    //    return converted;
+    //}
+   
     public static void SavetoFile(List<Highscore> players)
     {
         StreamWriter sw = new StreamWriter(DATAFILE);
         foreach (Highscore player in players)
         {
-            string output = player.PlayerName + "," + player.PlayerFinalScore;
+            string output = player.PlayerName + "," + player.PlayerFinalScore.ToString();
             sw.WriteLine(output);
         }
         sw.Flush();
@@ -44,23 +48,21 @@ public class Highscore
     }
     private static List<Highscore> OpenFromFile(string filename)
     {
-
         List<Highscore> list = new List<Highscore>();
         StreamReader sr = new StreamReader(filename);
         string line = sr.ReadLine();
-
+        
         while (line != null)
         {
             string[] record = line.Split(',');
-            Highscore contact = new Highscore(record[0], record[1]);
+            Highscore thehigh = new Highscore(record[0], int.Parse(record[1]), record[2]);
             
-            list.Add(contact);
+            list.Add(thehigh);
 
             line = sr.ReadLine();
 
         }
         sr.Close();
-        //OpenFromFile(line);
         return list;
     }
 
