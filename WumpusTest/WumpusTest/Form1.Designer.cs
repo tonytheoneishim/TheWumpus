045e8d38377b6edc1feb69033dd553408607f24d
TitanMaster7801 <changlew7801@gmail.com>
2025-05-01 14:50:19 -0700

    The actual Locations and Player modules

---
+++ WumpusTest/WumpusTest/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WumpusTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            playerConstructor = new Button();
+            locationsConstructor = new Button();
+            movePlayerRadio = new RadioButton();
+            playerBox = new GroupBox();
+            playerBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // playerConstructor
+            // 
+            playerConstructor.Location = new Point(12, 12);
+            playerConstructor.Name = "playerConstructor";
+            playerConstructor.Size = new Size(134, 63);
+            playerConstructor.TabIndex = 0;
+            playerConstructor.Text = "Player Constructor";
+            playerConstructor.UseVisualStyleBackColor = true;
+            playerConstructor.Click += playerConstructor_Click;
+            // 
+            // locationsConstructor
+            // 
+            locationsConstructor.Location = new Point(654, 12);
+            locationsConstructor.Name = "locationsConstructor";
+            locationsConstructor.Size = new Size(134, 63);
+            locationsConstructor.TabIndex = 1;
+            locationsConstructor.Text = "Locations Constructor";
+            locationsConstructor.UseVisualStyleBackColor = true;
+            locationsConstructor.Click += locationsConstructor_Click;
+            // 
+            // movePlayerRadio
+            // 
+            movePlayerRadio.AutoSize = true;
+            movePlayerRadio.Location = new Point(6, 22);
+            movePlayerRadio.Name = "movePlayerRadio";
+            movePlayerRadio.Size = new Size(90, 19);
+            movePlayerRadio.TabIndex = 2;
+            movePlayerRadio.TabStop = true;
+            movePlayerRadio.Text = "Move Player";
+            movePlayerRadio.UseVisualStyleBackColor = true;
+            // 
+            // playerBox
+            // 
+            playerBox.Controls.Add(movePlayerRadio);
+            playerBox.Location = new Point(12, 81);
+            playerBox.Name = "playerBox";
+            playerBox.Size = new Size(160, 179);
+            playerBox.TabIndex = 3;
+            playerBox.TabStop = false;
+            playerBox.Text = "Player Options:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(playerBox);
+            Controls.Add(locationsConstructor);
+            Controls.Add(playerConstructor);
+            Name = "Form1";
+            Text = "Form1";
+            playerBox.ResumeLayout(false);
+            playerBox.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button playerConstructor;
+        private Button locationsConstructor;
+        private RadioButton movePlayerRadio;
+        private GroupBox playerBox;
+    }
+}
---
+++ WumpusTest/WumpusTest/Locations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace WumpusTest
+{
+    public class Locations
+    {
+        public int Player { get; set; }
+        public List<int> Bats { get; set; }
+        public List<int> Pits { get; set; }
+        public List<int> Wumpus { get; set; }
+        public Locations(int player, List<int> bats, List<int> pits, List<int> wumpus)
+        {
+            Player = player;
+            Bats = bats;
+            Pits = pits;
+            Wumpus = wumpus;
+        }
+
+        public bool ShootArrow(int i)
+        {
+            // "i" is the cave index of where the arrow is being shot at
+            // Returns true if the arrow hits the Wumpus and false otherwise
+            return true;
+        }
+        public bool BatNearby(int i)
+        {
+            // "i" is the index of the cave the player is in
+            // Returns true if there's bats in the nearby caves and false otherwise
+            return true;
+        }
+
+        public bool PitNearby()
+        {
+            // Returns true if there's a pit in the nearby caves and false otherwise
+            return true;
+        }
+
+        public bool WumpusNearby()
+        {
+            // Returns true if the Wumpus in the nearby caves and false otherwise
+            return true;
+        }
+
+        public bool BuyHint(int gold)
+        {
+            // "gold" is the amount of gold the player has
+            // Returns true if "gold" is greater than or equal to the hint price and false otherwise
+            return true;
+        }
+
+        public List<int> WumpusLocation()
+        {
+            // Returns the cave index of the Wumpus's location
+            return Wumpus;
+        }
+
+        public int PlayerLocation()
+        {
+            // Returns the cave index of the player's location
+            return 1;
+        }
+
+        public int[] BatLocations()
+        {
+            // Returns an array of cave indexes where the bats are
+            return new int[] { 2, 3 };
+        }
+
+        public int[] PitLocations()
+        {
+            // Returns an array of cave indexes where the pits are
+            return new int[] { 4, 5 };
+        }
+
+        public void MoveWumpus(int i)
+        {
+            // "i" is the index of the cave to move the Wumpus to
+            // Moves the Wumpus to cave "i"
+        }
+    }
+}
