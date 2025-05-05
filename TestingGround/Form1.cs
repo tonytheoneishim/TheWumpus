09c238f62ed7cd1befbef70272fdc227d80561f2
greenshanghai <2003778@gmail.com>
2025-05-05 15:10:27 -0700

    Highscore testing

--- TestingGround/Form1.cs
+++ TestingGround/Form1.cs
@@ -6,10 +6,16 @@ namespace TestingGround
 {
     public partial class Form1 : Form
     {
-
+        Highscore highscore = new Highscore();
         public Form1()
         {
             InitializeComponent();
+
+            highscore.PlayerList.Add(new Highscore("Kellen1", 10, "Cave1"));
+            highscore.PlayerList.Add(new Highscore("Derek2", 20, "Cave2"));
+            highscore.PlayerList.Add(new Highscore("Maxim(3)", 30, "Cave3"));
+
+            Highscore.SavetoFile(highscore.PlayerList);
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
