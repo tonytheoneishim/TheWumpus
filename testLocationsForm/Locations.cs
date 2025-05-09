743c1af0a68ad4b1f294675abcd308e93614c839
TitanMaster7801 <changlew7801@gmail.com>
2025-05-09 14:40:06 -0700

    Commit

--- testLocationsForm/Locations.cs
+++ testLocationsForm/Locations.cs
@@ -11,25 +11,37 @@ namespace testLocationsForm
 {
     public class Locations
     {
-        public List<int[]> RoomPaths { get; set; } // The cave system
+        public List<int[]> Cave { get; set; } // The current cave system
         public int[] Bats { get; set; } // The indexes of the bats, 0-based
         public int[] Pits { get; set; } // The indexes of the pits, 0-based
         public int[] Shops { get; set; } // The indexes of the shops, 0-based
         public int Wumpus { get; set; } // The index of the Wumpus, 0-based
         public int Player { get; set; } // The index of the player, 0-based
 
-        public Locations(List<int[]> roomPaths, int[] bats, int[] pits, int wumpus, int player)
+        /// <summary>
+        /// Constructs a Location that stores the current cave system, the locations of the hazards, and the player's location
+        /// </summary>
+        /// <param name="Cave"> A list of caves and their connections </param>
+        /// <param name="bats"> A list of indexes where the bats are </param>
+        /// <param name="pits"> A list of indexes where the pits are </param>
+        /// <param name="wumpus"> The index where the Wumpus is </param>
+        /// <param name="player"> The index where the player is </param>
+        public Locations(List<int[]> cave, int[] bats, int[] pits, int wumpus, int player)
         {
-            RoomPaths = roomPaths;
+            Cave = cave;
             Bats = bats;
             Pits = pits;
             Wumpus = wumpus;
-
             Player = player;
         }
 
+        /// <summary>
+        /// Randomly moves the Wumpus to the nearest room, if moving at all
+        /// </summary>
+        /// <returns> Returns true if the Wumpus has moved and false otherwise </returns>
         public bool MoveWumpus()
-        { // Randomly decide if the Wumpus is moving
+        {
+            // Randomly decide if the Wumpus is moving
             Random rng = new Random();
             if (rng.Next(0, 2) == 0)
             {
@@ -39,7 +51,7 @@ namespace testLocationsForm
             // Randomly decide which direction the Wumpus will move towards
             int count = 0;
             List<int> paths = new List<int>();
-            foreach (int path in RoomPaths[Wumpus])
+            foreach (int path in Cave[Wumpus])
             {
                 if (path != -1)
                 {
@@ -50,20 +62,29 @@ namespace testLocationsForm
 
             Wumpus = paths[rng.Next(0, count)];
             return true;
-        } // Returns true if the Wumpus moved and false if it didn't
+        }
 
-        public bool ShootArrow(int cave)
-        { // Shoots an arrow at "cave"
-            if (cave == Wumpus)
+        /// <summary>
+        /// Shoots an arrow at the selected cave
+        /// </summary>
+        /// <param name="cave"> The index of the cave to shoot at </param>
+        /// <returns> Returns true if you shot the Wumpus and false otherwise </returns>
+        public bool ShootArrow(int i)
+        {
+            if (i == Wumpus)
             {
                 return true;
             }
 
             return false;
-        } // Returns true if the arrow hit the Wumpus and false if it didn't
+        }
 
+        /// <summary>
+        /// Checks the current room for hazards or a shop
+        /// </summary>
+        /// <returns> Returns W if there's a Wumpus, B if there's a bat, P if there's a pit, S if there's a shop, and N otherwise </returns>
         public char RoomType()
-        { // Checks the type of room at index "i"
+        {
             if (Player == Wumpus)
             {
                 return 'W';
@@ -94,15 +115,20 @@ namespace testLocationsForm
             }
 
             return 'N';
-        } // Returns W if it's the Wumpus, B if it's a bat, P if it's a pit, S if it's a shop, and N if it's a normal room
+        }
 
+        /// <summary>
+        /// Checks the nearby caves for hazards or a shop
+        /// </summary>
+        /// <returns> Returns W if there's a Wumpus, B if there's a bat, P if there's a pit, S if there's a shop, and N otherwise </returns>
         public char HazardNearby()
-        { // Checks for hazards in the caves around the player
-            for (int i = 0; i < RoomPaths[Player].Length; i++)
+        {
+            // Check each room connection one by one
+            for (int i = 0; i < Cave[Player].Length; i++)
             {
                 foreach (int bat in Bats)
                 {
-                    if (RoomPaths[Player][i] == bat)
+                    if (Cave[Player][i] == bat)
                     {
                         return 'B';
 
@@ -111,7 +137,7 @@ namespace testLocationsForm
 
                 foreach (int pit in Pits)
                 {
-                    if (RoomPaths[Player][i] == pit)
+                    if (Cave[Player][i] == pit)
                     {
                         return 'P';
                     }
@@ -119,19 +145,19 @@ namespace testLocationsForm
 
                 foreach (int shop in Shops)
                 {
-                    if (RoomPaths[Player][i] == shop)
+                    if (Cave[Player][i] == shop)
                     {
                         return 'S';
                     }
                 }
 
-                if (RoomPaths[Player][i] == Wumpus)
+                if (Cave[Player][i] == Wumpus)
                 {
                     return 'W';
                 }
             }
 
             return 'N';
-        } // Returns B if there's a bat nearby, P if there's a pit nearby, S if there's a shop nearby, W is there's a Wumpus nearby, and N otherwise
+        }
     }
 }
