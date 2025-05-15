66aa4b5ad978cf419fda99f20305e69e16b834a9
tonytheoneishim <2034264@apps.nsd.org>
2025-05-15 15:06:33 -0700

    fixed not useign all questions

---
+++ Trivia/Prompt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia
+{
+    public class Prompt
+    {
+        public string Question { get; set; }
+        public string[] Options { get; set; }
+        public string Answer { get; set; }
+        public Prompt(string UnParsed) 
+        {
+
+            string[] Line = UnParsed.Split(',');
+            Question = Line[0];
+            Answer = Line[1];
+            Options = Line.Skip(1).ToArray();
+
+            
+
+        }
+     
+    }
+}
