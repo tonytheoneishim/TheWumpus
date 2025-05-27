aedcac8610ab3f5784b20f172612aea9655c1518
TitanMaster7801 <changlew7801@gmail.com>
2025-05-27 15:05:24 -0700

    Commit

--- testLocationsForm/Locations.cs
+++ testLocationsForm/Locations.cs
@@ -43,18 +43,17 @@ namespace WumpusLocations
             int wumpus;
             List<int> indexes = new List<int>(29);
             List<int> events = new List<int>();
-            for (int i = 0; i < 29; i++)
+            for (int i = 0; i < 30; i++)
             {
                 indexes.Add(i);
             }
 
             Random rng = new Random();
-            for (int i = 0; i <= 6; i++)
+            for (int i = 1; i <= 6; i++)
             {
                 int x = rng.Next(1, 30 - i);
                 events.Add(indexes[x]);
                 indexes.RemoveAt(x);
-                indexes.Add(events[i]);
             }
 
             bats[0] = events[0];
@@ -151,7 +150,7 @@ namespace WumpusLocations
             {
                 foreach (int path in CavePaths[Player])
                 {
-                    if (path == i)
+                    if (path - 1 == i)
                     {
                         return true;
                     }
@@ -249,8 +248,10 @@ namespace WumpusLocations
                     hazards.Add("W");
                     break;
                 }
-
-                hazards.Add("N");
+                if (!(CavePaths[Player][i] - 1 == -1))
+                {
+                    hazards.Add("N");
+                }
             }
 
             return hazards;
